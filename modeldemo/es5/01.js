3286cb84afa07f82b918fd8c6c9a1996734a418d
xinyang <1349746992@qq.com>
2023-05-18 14:26:17 +0800

    1.0.04

---
+++ modeldemo/es5/01.js
@@ -0,0 +1,12 @@
+// 定义成员：
+const sum = function (a, b) {
+    return parseInt(a) + parseInt(b)
+}
+const subtract = function (a, b) {
+    return parseInt(a) - parseInt(b)
+}
+//设置哪些方法可以被其他的js调用
+module.exports = {
+    sum,
+    subtract
+}
